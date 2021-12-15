8130857ee78ec5911882c678c32ad2e96c7639df
ElizaUs <96049558+ElizaUs@users.noreply.github.com>
2021-12-15 11:02:55 +0300

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -11,20 +11,12 @@ class MyParser:
         current_date = re.search("\d\d\W\d\d\W\d\d\d\d\s", f_str)
         print("\nCurrent date: " + current_date.group())
 
-        # get available codes
-        raw_codes = re.findall(">""[A-Z]{3}", f_str)
-
-        # create new list with correct currency
-        currency_list = []
-        for x in raw_codes:
-            y = x.replace('>', '')
-            currency_list.append(y)
+       # get available codes
+        currency_list = re.findall("(?<=td>)[A-Z]{3}", f_str)
 
         # get available rates
-        currency_values = re.findall("[0-9]{2}[,][0-9]{4}", f_str)
-        currency_values.remove("50,2224")
+        currency_values = re.findall("(?<=td>)[0-9]{2}[,][0-9]{4}", f_str)
         #create dictionary for currency-currency value
-        #global dictionary
         dictionary = dict(zip(currency_list,currency_values))
         print(dictionary)
         return dictionary
